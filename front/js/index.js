dd1e09d34c3680ebe7d3aaa5b2ed8d955fe83ef1
Mathieu Franchi <math13300@live.fr>
2022-01-24 23:26:30 +0100

    Indentation index.js

--- front/js/index.js
+++ front/js/index.js
@@ -1,48 +1,47 @@
 fetch('http://localhost:3000/api/products')//requete de type get sur l'ensemble des produits 
-     .then (function(res)
-     {  //récupérer le résultat de la requête
-       if (res.ok)
-       {
-          return res.json();//résultat au format json
-       }
-     })
-     .then(function(products)
-    {//products objet renvoyé, promise a result
-      for (prod of products)
-      {
+     
+.then (function(res)
+{  //récupérer le résultat de la requête
+  if (res.ok)
+    {
+      return res.json(); //résultat au format json
+    }
+})
 
-      
+.then(function(products)
+{//products objet renvoyé, promise a result
+  for (prod of products)
+    {
+      const section = document.getElementById("items");
+      a = document.createElement('a');
+      article = document.createElement('article');
+            article.classList.add("productCard");
+      img = document.createElement('img');
+            img.classList.add('productImage');
+      h3 = document.createElement('h3');
+            h3.classList.add("productName");
+      p = document.createElement('p');
+            p.classList.add("productDescription");
 
-     const section = document.getElementById("items");
-     a = document.createElement('a');
-     article = document.createElement('article');
-         article.classList.add("productCard");
-     img = document.createElement('img');
-         img.classList.add('productImage');
-     h3 = document.createElement('h3');
-         h3.classList.add("productName");
-     p = document.createElement('p');
-         p.classList.add("productDescription");
-
-        
-         items.appendChild(a);
-         a.appendChild(article);
-         article.appendChild(img);
-         article.appendChild(h3);
-         article.appendChild(p);
+            
+            items.appendChild(a);
+            a.appendChild(article);
+            article.appendChild(img);
+            article.appendChild(h3);
+            article.appendChild(p);
 
             // On inject le a dans le targetContainer
 
-    //fin boucle
-    console.log(products._id);
-    // a.innerHTML =(value);
+            //fin boucle
+            console.log(products._id);
+            // a.innerHTML =(value);
 
-    a.href = "./product.html?id=" + prod._id;
-    img.src= prod.imageUrl;
-    h3.innerHTML = prod.name;
-    img.alt = prod.altTxt;
-    p.innerHTML = prod.description;
-  }
+            a.href = "./product.html?id=" + prod._id;
+            img.src= prod.imageUrl;
+            h3.innerHTML = prod.name;
+            img.alt = prod.altTxt;
+            p.innerHTML = prod.description;
+    }
 
 })
 
