da15422c37ba3a499d0dbc58f1f4d12b20c96306
Mathieu Franchi <math13300@live.fr>
2022-02-04 04:43:01 +0100

    product  js and index js update

--- front/js/index.js
+++ front/js/index.js
@@ -1,15 +1,15 @@
-fetch('http://localhost:3000/api/products')//requete de type get sur l'ensemble des produits 
+const dataApi = fetch('http://localhost:3000/api/products');
+//requete de type get sur l'ensemble des produits 
      
-.then (function(res)
+dataApi.then (function(res)
 {  //récupérer le résultat de la requête
-  if (res.ok)
     {
       return res.json(); //résultat au format json
     }
 })
 
 .then(function(products)
-{//products objet renvoyé, promise a result
+{ //products objet renvoyé, promise a result
   for (prod of products)
     {
       const section = document.getElementById("items");
@@ -30,19 +30,16 @@ fetch('http://localhost:3000/api/products')//requete de type get sur l'ensemble 
             article.appendChild(h3);
             article.appendChild(p);
 
-            // On inject le a dans le targetContainer
-
-            //fin boucle
-            console.log(products._id);
-            // a.innerHTML =(value);
-
+            
+            
             a.href = "./product.html?id=" + prod._id;
-            img.src= prod.imageUrl;
+            img.src = prod.imageUrl;
             h3.innerHTML = prod.name;
             img.alt = prod.altTxt;
             p.innerHTML = prod.description;
     }
 
 })
+
 
 
--- front/js/product.js
+++ front/js/product.js
@@ -3,17 +3,68 @@ const urlParams = new URLSearchParams(queryString);
 
 const id = urlParams.get('id');
 
-fetch('http://localhost:3000/api/products/'+ id) //requete de type get sur l'ensemble des produits 
+
+fetch('http://localhost:3000/api/products/'+ id)
+//requete de type get sur l'url correspondant au produit choisis
 
 .then (function(res)
-     {  //récupérer le résultat de la requête
-       
-       
-          return res.json();//résultat au format json
-       
+     {    //récupérer le résultat de la requête
+           return res.json(); //résultat au format json
      })
-     .then(function(product)
-    {//products objet renvoyé, promise a result
-      console.log(product.price)
-    }
-            );
+     
+.then (function(product)
+     {//products objet renvoyé, promise a result
+         const itemImg = document.querySelector(".item__img");
+         const img = document.createElement('img');
+         img.setAttribute('src',product.imageUrl);
+         itemImg.appendChild(img);
+
+         document.getElementById("price").textContent = product.price;
+         document.getElementById("description").textContent = product.description;
+         
+          let select  = document.getElementById("colors");
+			// recup couleurs differentes selon kanap
+          const colors = product.colors;
+                 
+                  
+		     //add color from specific kanap in combolist  
+		for (let i = 0; i < colors.length; i += 1) 
+          {
+               let selectOption = document.createElement('option');
+                 	
+               selectOption.value = colors[i];
+               selectOption.text = colors[i];
+                      
+                      
+			select.add(selectOption);
+               
+          }
+          
+          let quantityChoose = 0;
+				document.getElementById("quantity").addEventListener("change", function() {
+  					quantityChoose = this.value;
+  				});
+				
+				console.log(quantityChoose); //mode debug
+          
+          const btnAddCart = document.getElementById("addToCart");
+          btnAddCart.addEventListener
+			     (  "click", function()
+				{
+					//add in basket but do not redirect in panier.html to redirect click on link in header
+					let productInBasket = 
+				     {
+			        	kanapName: product.name,
+			          kanapId: product._id,
+			          kanapPrice: product.price,
+					kanapImg: product.imageUrl,
+					kanapColor: select.value,
+			          quantity: quantityChoose
+			          };
+			      	
+                         addBasket(productInBasket);
+                         localStorage.setItem("basket",JSON.stringify(basket));
+			     }
+                    );
+         
+     });
